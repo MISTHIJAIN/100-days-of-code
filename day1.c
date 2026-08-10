172402671ae5da794cb839cb7fe0a25181f4eb77
Misthi Jain <https://github.com/MISTHIJAIN/100-days-of-code.git>
2026-08-10 17:29:24 +0000

    Day 1: Add sum of two numbers program

---
+++ day1.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include <stdio.h>
+//Q1: Write a program to input two numbers and display their sum.
+
+int main()
+{
+int a, b,sum;
+printf("enter the number a and b=");
+scanf("%d %d",&a,&b);
+sum= a+b;
+printf("%d",sum);
+return 0;
+}
+

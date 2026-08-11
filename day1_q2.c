5c1141abbc5d2bbd5dc40f1afae17675fb3a4442
Misthi Jain <https://github.com/MISTHIJAIN/100-days-of-code.git>
2026-08-11 17:49:08 +0000

    Add Day 1 question 2 solution

---
+++ day1_q2.c
@@ -0,0 +1,14 @@
+ //Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the two numbers- ");
+    scanf("%d %d", &a,&b);
+    printf("Sum: %d\n", a+b);
+    printf("Difference: %d\n", a-b);
+    printf("Product: %d\n", a*b);
+    printf("Quotient: %.2f\n", (float)a/b);
+    return 0;
+}

b76f80106bad8b3139e9f57651a5a6f7ebaafa97
Misthi Jain <https://github.com/MISTHIJAIN/100-days-of-code.git>
2026-08-12 16:41:54 +0000

    Add Day 3 question 6 solution

---
+++ day3_q6.c
@@ -0,0 +1,14 @@
+//Q6: Write a program to swap two numbers using a third variable.
+
+#include <stdio.h>
+int main()
+{
+    int a,b,x;
+    printf("Enter two numbers-");
+    scanf("%d %d",&a,&b);
+    x=a;
+    a=b;
+    b=x;
+    printf("After swap: %d %d",a,b);
+    return 0;
+}
